0f854f90b4af8fd20b2685e60a71c5e7e4567006
Sam Grose <samugrose@gmail.com>
2020-07-08 14:14:29 -0700

    initiated npm, installed inquirer, added required classes to index

---
+++ index.js
@@ -0,0 +1,6 @@
+var employee = require("employee.js");
+var manager = require("manager.js");
+var engineer = require("engineer.js");
+var intern = require("intern.js");
+
+var inquirer = require"inquirer";
