748b85c07f3ad16dcb875666175ece56730dccc2
Sam Grose <samugrose@gmail.com>
2020-07-08 14:18:52 -0700

    added fs, fied inquirer var

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ var manager = require("manager.js");
 var engineer = require("engineer.js");
 var intern = require("intern.js");
 
-var inquirer = require"inquirer";
+var fs = require("fs");
+var inquirer = require("inquirer");
+
